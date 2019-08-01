f9dd29a2c0d6d0c528f8af5ca9eb9176660ec73e
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-08-01 06:33:03 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningOfArray(array,element){
 }
 
 function destructivelyAddElementToBeginningOfArray(array,element){
-  array = array.unshift(element)
+  array.unshift(element)
   return array 
 }
 
@@ -17,6 +17,6 @@ function addElementToEndOfArray(array,element){
 }
 
 function destructivelyAddElementToEndOfArray(array,element){
-  array = array.push(element)
+  array.push(element)
   return array 
 }
